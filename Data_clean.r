49620216650bb81d6beac7140086e46043636138
Eric <pyrophage@gmail.com>
2022-03-19 15:25:05 -0400

    Clean up variable names to be more R friendly

--- Data_clean.r
+++ Data_clean.r
@@ -5,17 +5,25 @@ library(httr)
 url<-"https://www2.illinois.gov/idoc/reportsandstatistics/Documents/December%202021%20Prison%20Stock.xls"
 
 GET(url, write_disk(tf <-tempfile(fileext = ".xls")))
-df<-read_excel(tf, skip = 5)
+df<-read_excel(tf, skip = 5)%>%
+  rename(DOCNUM = `IDOC #`, DOB = `Date of Birth`, Veteran = `Veteran Status`, Curr_adm_dt = `Current Admission Date`,
+         Admit_type = `Admission Type`, Parent_Institution = `Parent Institution`, MSR_dt = `Projected Mandatory Supervised Release (MSR) Date3`,
+         Proj_Disc_dt = `Projected Discharge Date3`, Custody_dt = `Custody Date`, Sent_dt = `Sentence Date`,
+         Crime_Class = `Crime Class`, Holding_Offense = `Holding Offense`, HO_Cat = `Holding Offense Category`, 
+         Offense_Type = `Offense Type`, Sent_yrs = `Sentence Years`, Sent_mon = `Sentence Months`,
+         Truth_in_Sentencing = `Truth in Sentencing`, Sent_County = `Sentencing County`)
 
 life<-df%>%
-  filter(`Sentence Years` == "LIFE")
+  filter(Sent_yrs == "LIFE")
 
 SDP<-df%>%
-  filter(`Sentence Years` == "SDP")
+  filter(Sent_yrs == "SDP")
 
 Standard<-df%>%
-  filter(!`Sentence Years` %in% c("LIFE", "SDP"))%>%
-  mutate(`Sentence Years` = as.numeric(`Sentence Years`))
+  filter(!Sent_yrs %in% c("LIFE", "SDP"))%>%
+  filter(!is.na(Name))%>%
+  mutate(Sent_yrs = as.numeric(Sent_yrs), 
+         Sent_mon = as.numeric(Sent_mon))
 
 
 Race<-life%>%
