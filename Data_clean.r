b75f00454ff70fb401a49f893817429f3dce5734
Eric <pyrophage@gmail.com>
2022-03-19 14:54:57 -0400

    add race summary for life sentences

--- Data_clean.r
+++ Data_clean.r
@@ -17,3 +17,7 @@ Standard<-df%>%
   filter(!`Sentence Years` %in% c("LIFE", "SDP"))%>%
   mutate(`Sentence Years` = as.numeric(`Sentence Years`))
 
+
+Race<-life%>%
+  group_by(Race)%>%
+  summarise(Count = n())
