2778dbe30fbe4e6ffb9779faf66336623546927f
Eric <pyrophage@gmail.com>
2022-03-19 08:53:08 -0400

    load file

---
+++ Data_clean.r
@@ -0,0 +1,19 @@
+library(readxl)
+library(dplyr)
+library(httr)
+
+url<-"https://www2.illinois.gov/idoc/reportsandstatistics/Documents/December%202021%20Prison%20Stock.xls"
+
+GET(url, write_disk(tf <-tempfile(fileext = ".xls")))
+df<-read_excel(tf, skip = 5)
+
+life<-df%>%
+  filter(`Sentence Years` == "LIFE")
+
+SDP<-df%>%
+  filter(`Sentence Years` == "SDP")
+
+Standard<-df%>%
+  filter(!`Sentence Years` %in% c("LIFE", "SDP"))%>%
+  mutate(`Sentence Years` = as.numeric(`Sentence Years`))
+
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Illinois Corrections
 
 Testing a repository for creating code to work on corrections data. 
+
+Pulling data from the publically available prisoner data set on the Illinois DOC website for December 2021. 
